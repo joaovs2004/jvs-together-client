fd83368226963e32e9aacc0ea65ab40ca7e1c1ec
mateusrdc <mateus@e.email>
2023-08-13 18:13:46 -0300

    Update dependencies

--- src/stores.ts
+++ src/stores.ts
@@ -1,10 +1,10 @@
 import { Writable, writable } from "svelte/store";
 import type { playerProvider } from "./playerProvider";
-import type { ComponentType, SvelteComponentTyped } from "svelte";
+import type { ComponentType, SvelteComponent } from "svelte";
 import YoutubePlayer from './lib/players/YoutubePlayer.svelte';
 
 export const player: Writable<playerProvider> = writable();
-export const playerComponent: Writable<ComponentType<SvelteComponentTyped & playerProvider>> = writable(YoutubePlayer);
+export const playerComponent: Writable<ComponentType<SvelteComponent & playerProvider>> = writable(YoutubePlayer);
 export const playing: Writable<boolean> = writable(false);
 export const roomId = writable();
 export const clientId = writable();
